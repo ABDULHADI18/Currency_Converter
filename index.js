efc797a009bf6689252438ae27cd5a53c22e048d
ABDULHADI18 <160162625+ABDULHADI18@users.noreply.github.com>
2024-04-14 20:14:43 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,39 @@
+#! /usr/bin/env node
+import chalk from "chalk";
+import inquirer from "inquirer";
+console.log(chalk.greenBright("WELCOME TO CURRENCY CONVERTER"));
+const currency = {
+    USD: 1, //base currency
+    EUR: 0.94,
+    GBP: 0.80,
+    INR: 83.61,
+    PKR: 277.30,
+    DINAR: 0.31,
+    DIRHAM: 3.67,
+};
+let user_answer = await inquirer.prompt([
+    {
+        name: "from",
+        message: "Enter From Currency",
+        type: "list",
+        choices: ["USD", "EUR", "GBP", "INR", "PKR", "DINAR", "DIRHAM"]
+    },
+    {
+        name: "to",
+        message: "Enter To Currency",
+        type: "list",
+        choices: ["USD", "EUR", "GBP", "INR", "PKR", "DINAR", "DIRHAM"]
+    },
+    {
+        name: "amount",
+        message: "Enter Your Amount",
+        type: "number",
+    },
+]);
+let fromAmount = currency[user_answer.from]; // excange rate
+let toAmount = currency[user_answer.to]; // excange rate
+let amount = user_answer.amount;
+let baseAmount = amount / fromAmount; // USD base currency
+let convertedAmount = baseAmount * toAmount;
+console.log(chalk.blue(convertedAmount));
+console.log(chalk.red("THANK YOU FOR USING CURRENCY_CONVERTER"));
